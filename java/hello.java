0fcc58921c89e83381d18e87b0f3bf257249ea93
krishnaawasthi-26 <krishnaawasthi2005@gmail.com>
2025-03-02 18:40:29 +0530

    Add files via upload

---
+++ java/hello.java
@@ -0,0 +1,8 @@
+class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int var1 = 5;
+        // int var2 = 10;
+        System.out.println(var1);
+    }
+}
